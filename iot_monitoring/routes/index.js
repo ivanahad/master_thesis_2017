3c8d44f08838d11764c23d8c9ab288e129fe7fc7
Ndizera Eddy <eddyndizera@gmail.com>
2017-03-16 10:47:16 +0100

    receive random traffic values periodically

--- iot_monitoring/routes/index.js
+++ iot_monitoring/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/data', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  var value = Math.random() * 100;
+  res.send(JSON.stringify({ a: value }));
+});
+
 module.exports = router;
