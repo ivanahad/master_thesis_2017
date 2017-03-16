1b936bf4bff668d1f00fd180f81ba2c2c21a54c0
ivanahad <ivan.abdelahad@uclouvain.be>
2017-03-16 12:29:42 +0100

    update index.js

--- iot_monitoring/routes/index.js
+++ iot_monitoring/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/coucou', function(req, res, next) {
+    res.render('map', {});
+});
+
+router.get('/salut', function(req, res, next) {
+    res.render('directed', {});
+});
+
+router.get('/test1', function(req, res, next) {
+    res.render('chart', {});
+});
+
+router.get('/test2', function(req, res, next) {
+    res.render('network', {});
+});
+
+router.get('/test3', function(req, res, next) {
+    res.render('courbe', {});
+});
+
+
 module.exports = router;
