0e8b43d1b2aa5229de06250457180c3e7f622c82
Ndizera Eddy <eddyndizera@gmail.com>
2017-03-22 17:08:07 +0100

    added navigation bar

--- iot_monitoring/routes/index.js
+++ iot_monitoring/routes/index.js
@@ -3,21 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
 });
-
-router.get('/coucou', function(req, res, next) {
-    res.render('map', {});
-});
-
-router.get('/salut', function(req, res, next) {
-    res.render('directed', {});
-});
-
-router.get('/test1', function(req, res, next) {
-    res.render('chart', {});
-});
-
 
 router.get('/data', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
