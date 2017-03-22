e165f8dff010b35f349a0501a2b56850d55e9c41
Ndizera Eddy <eddyndizera@gmail.com>
2017-03-22 21:07:18 +0100

    refactored name of title and url

--- iot_monitoring/routes/index.js
+++ iot_monitoring/routes/index.js
@@ -6,10 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', {});
 });
 
-router.get('/data', function(req, res, next) {
+router.get('/traffic_volume', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   var value = Math.random() * 100;
-  res.send(JSON.stringify({ a: value }));
+  res.send(JSON.stringify({ volume: value }));
 });
 
 module.exports = router;
