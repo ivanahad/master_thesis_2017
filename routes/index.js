0c74e722fe5954df7843dba11d22431389c1af2a
Ndizera Eddy <eddyndizera@gmail.com>
2017-04-25 18:28:19 +0200

    restructured repo

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,20 @@ router.get('/traffic_volume', function(req, res, next) {
     {date: new Date(2007, 3, 26), volume: 5},
     {date: new Date(2007, 3, 27), volume: 15},
     {date: new Date(2007, 3, 30), volume: 23},
-    {date: new Date(2007, 4,  1), volume: 17},
+    {date: new Date(2007, 4,  1), volume: 17}
+  ];
+  res.send(JSON.stringify(data));
+});
+
+router.get('/nodes_link', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  var data = [
+    {source: 1, destination: 2},
+    {source: 1, destination: 3},
+    {source: 2, destination: 4},
+    {source: 3, destination: 4},
+    {source: 4, destination: 5},
+    {source: 5, destination: 6}
   ];
   res.send(JSON.stringify(data));
 });
