24613e17e14974c85fad95df71e6596a9055098d
Ndizera Eddy <eddyndizera@gmail.com>
2017-04-26 18:13:53 +0200

    parse ipfix msg with fixed templates

--- libs/ipfix.js
+++ libs/ipfix.js
@@ -1,28 +1,55 @@
-const Parser = require('binary-parser').Parser
+const Parser = require('binary-parser').Parser;
 
 function Ipfix(){}
 
-const HEADER_LENGTH = 16
+Ipfix.HEADER_LENGTH = 16;
 
-const ipfixHeader = new Parser()
-    .endianess('big')
-    .uint16('version')
-    .uint16('length')
-    .uint32('exportTime')
-    .uint32('seqNo')
-    .uint32('domainId');
-
+// TODO throw errors
 Ipfix.prototype.parse = function (binaryBuffer) {
-  if(binaryBuffer == undefined){
+  if(!binaryBuffer){
     return null;
   }
-  else if (binaryBuffer.length < HEADER_LENGTH) {
+  else if (binaryBuffer.length < Ipfix.HEADER_LENGTH) {
     return null;
   }
   else{
-    return ipfixHeader.parse(binaryBuffer);
-
+    const copyBuffer = Buffer.alloc(binaryBuffer.length);
+    binaryBuffer.copy(copyBuffer);
+    return ipfixMsg.parse(copyBuffer);
   }
 };
+
+const ipfixInfoElem = new Parser()
+    .uint16('id')
+    .uint16('length');
+
+const ipfixTemplate = new Parser()
+    .uint16('id')
+    .uint16('count')
+    .array('elements', {
+      type: ipfixInfoElem,
+      length: 2
+    });
+
+const ipfixSet = new Parser()
+  .uint16('id')
+  .uint16('length')
+  .array('templates', {
+    type: ipfixTemplate,
+    length: 1
+  });
+
+const ipfixMsg = new Parser()
+  // Header
+  .uint16('version')
+  .uint16('length')
+  .uint32('exportTime')
+  .uint32('seqNo')
+  .uint32('domainId')
+  // Sets
+  .array('sets', {
+    type: ipfixSet,
+    readUntil: 'eof'
+  });
 
 module.exports = Ipfix;
--- tests/ipfix.js
+++ tests/ipfix.js
@@ -3,9 +3,7 @@ const expect = chai.expect;
 const Ipfix = require('../libs/ipfix');
 
 // Tests data
-const ipfixHeader = createIpfixMessage(10, 16, 45, 1, 1);
-
-
+const ipfixHeader = createIpfixHeader(10, 16, 45, 1, 1);
 
 describe('Ipfix header parsing', function(){
   it('should return null if no data is passed', function(){
@@ -44,13 +42,78 @@ describe('Ipfix header parsing', function(){
   });
 });
 
-function createIpfixMessage(version, length, exportTime, seqNo, domainId){
+const ipfixTemplateMsg1 = Buffer.concat([
+  createIpfixHeader(10, 28, 1052, 1, 1),
+  createIpfixSetHeader(2, 12),
+  createIpfixSetTemplate(256, 2),
+  createIpfixInfoElem(1, 8),
+  createIpfixInfoElem(2, 8)
+], 32);
+
+describe('Ipfix template sets parsing', function(){
+  it('should parse correctly set header id', function(){
+    var ipfix = new Ipfix();
+    var ipfix_obj = ipfix.parse(ipfixTemplateMsg1);
+    expect(ipfix_obj.sets[0].id).to.equal(2);
+  });
+  it('should parse correctly set header length', function(){
+    var ipfix = new Ipfix();
+    var ipfix_obj = ipfix.parse(ipfixTemplateMsg1);
+    expect(ipfix_obj.sets[0].length).to.equal(12);
+  });
+  it('should parse correctly template set id', function(){
+    var ipfix = new Ipfix();
+    var ipfix_obj = ipfix.parse(ipfixTemplateMsg1);
+    expect(ipfix_obj.sets[0].templates[0].id).to.equal(256);
+  });
+  it('should parse correctly template set fields count', function(){
+    var ipfix = new Ipfix();
+    var ipfix_obj = ipfix.parse(ipfixTemplateMsg1);
+    expect(ipfix_obj.sets[0].templates[0].count).to.equal(2);
+  });
+  it('should parse correctly information elements', function(){
+    var ipfix = new Ipfix();
+    var ipfix_obj = ipfix.parse(ipfixTemplateMsg1);
+    expect(ipfix_obj.sets[0].templates[0].elements).to.have.lengthOf(2);
+    expect(ipfix_obj.sets[0].templates[0].elements[0].id).to.equal(1);
+    expect(ipfix_obj.sets[0].templates[0].elements[0].length).to.equal(8);
+    expect(ipfix_obj.sets[0].templates[0].elements[1].id).to.equal(2);
+    expect(ipfix_obj.sets[0].templates[0].elements[1].length).to.equal(8);
+  });
+
+});
+
+function createIpfixHeader(version, length, exportTime, seqNo, domainId){
   var msg = Buffer.alloc(16);
   msg.writeUInt16BE(version, 0); // IPFIX version
   msg.writeUInt16BE(length, 2); // IPFIX length
   msg.writeUInt32BE(exportTime, 4); // IPFIX Export time
   msg.writeUInt32BE(seqNo, 8); // IPFIX sequence number
   msg.writeUInt32BE(domainId, 12); // IPFIX domain ID
+  return msg;
+}
 
+function createIpfixSetHeader(id, length){
+  var msg = Buffer.alloc(4);
+  msg.writeUInt16BE(id, 0);
+  msg.writeUInt16BE(length, 2);
+
+  return msg;
+}
+
+function createIpfixSetTemplate(id, fieldCount){
+  return createIpfixSetHeader(id, fieldCount);
+}
+
+function createIpfixInfoElem(id, length, eid){
+  var msg = Buffer.alloc(4);
+  if(eid !== undefined){
+    msg = Buffer.alloc(8);
+  }
+  msg.writeUInt16BE(id, 0);
+  msg.writeUInt16BE(length, 2);
+  if(eid !== undefined){
+    msg.writeUInt32BE(eid, 4);
+  }
   return msg;
 }
