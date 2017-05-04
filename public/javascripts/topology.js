4564b87c0125df9d7c7cc9ac947c21196c120a9e
ivanahad <ivan.abdelahad@uclouvain.be>
2017-05-04 16:48:03 +0200

    la topologie s'affiche en fonction de ce qui est dans le fichier json

--- public/javascripts/topology.js
+++ public/javascripts/topology.js
@@ -10,64 +10,9 @@ var svg = d3.select("svg"),
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-
-// var links = [
-//     {source: "ID1", target: "ID2", type: "licensing"},
-//     {source: "ID1", target: "ID0", type: "licensing"},
-//     {source: "ID3", target: "ID4", type: "suit"},
-//     {source: "ID10", target: "ID4", type: "suit"},
-//     {source: "ID5", target: "ID4", type: "resolved"},
-//     {source: "ID0", target: "ID4", type: "suit"},
-//     {source: "ID7", target: "ID4", type: "suit"},
-//     {source: "ID4", target: "ID0", type: "suit"},
-//     {source: "ID3", target: "ID7", type: "resolved"},
-//     {source: "ID6", target: "ID7", type: "resolved"},
-//     {source: "RIM", target: "ID7", type: "suit"},
-//     {source: "ID7", target: "ID6", type: "resolved"},
-//     {source: "ID4", target: "ID5", type: "resolved"},
-//     {source: "ID9", target: "ID5", type: "resolved"},
-//     {source: "ID4", target: "ID10", type: "suit"},
-//     {source: "ID1", target: "ID10", type: "suit"},
-//     {source: "ID10", target: "ID1", type: "suit"},
-//     {source: "ID12", target: "ID11", type: "suit"},
-//     {source: "ID14", target: "ID11", type: "suit"},
-//     {source: "ID7", target: "ID3", type: "resolved"},
-//     {source: "ID4", target: "ID3", type: "suit"},
-//     {source: "ID7", target: "RIM", type: "suit"},
-//     {source: "ID5", target: "ID9", type: "suit"}
-// ];
-
-
-var links = [
-    {source: "ID1", target: "ID2"},
-    {source: "ID1", target: "ID0"},
-    {source: "ID3", target: "ID4"},
-    {source: "ID10", target: "ID4"},
-    {source: "ID5", target: "ID4"},
-    {source: "ID0", target: "ID4"},
-    {source: "ID7", target: "ID4"},
-    {source: "ID4", target: "ID0"},
-    {source: "ID3", target: "ID7"},
-    {source: "ID6", target: "ID7"},
-    {source: "RIM", target: "ID7"},
-    {source: "ID7", target: "ID6"},
-    {source: "ID4", target: "ID5"},
-    {source: "ID9", target: "ID5"},
-    {source: "ID4", target: "ID10"},
-    {source: "ID1", target: "ID10"},
-    {source: "ID10", target: "ID1"},
-    {source: "ID12", target: "ID11"},
-    {source: "ID14", target: "ID11"},
-    {source: "ID7", target: "ID3"},
-    {source: "ID4", target: "ID3"},
-    {source: "ID7", target: "RIM"},
-    {source: "ID5", target: "ID9"}
-];
-
-
 // var nodesbis = [];
-// function parseData(data){
+
+// function parseNodes(data){
 //     data.forEach(function(d) {
 //         var source = d.source;
 //         var target = d.target;
@@ -76,100 +21,32 @@ var links = [
 //     })
 // }
 
-// function parseDate(data){
-//     data.forEach(function (d) {
-//         var dateObj = new Date(d.date);
-//         d.date = dateObj;
-//         return d;
-//     });
-// }
-
-console.log("coucou");
-
 var links = [];
-
-
 var nodes = [];
+var nodestest = [
+    {ID : 1, parent : 2, battery : 52}
+];
 
+
+function parseLinks(data){
+    data.forEach(function(d) {
+        var sourced = d.source.toString();
+        var targetd = d.target.toString();
+        links.push({source : sourced, target : targetd })
+
+    });
+    links.forEach(function(link) {
+        link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+        link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+    });
+
+}
 //
-// function parseLinks(data){
-//     data.forEach(function(d) {
-//         var sourced = d.source.toString();
-//         var targetd = d.target.toString();
-//         links.push({source : sourced, target : targetd })
 //
-//     });
-//     console.log(links.length)
-//     links.forEach(function(link) {
-//         link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-//         link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-//     });
-//
-// }
-
-console.log("taille des links " + links.length);
-
-
-
-/*
-Parse correctement les donnees, les nodes et les links sont bien initialisés. Mais les tableaux ne sont initialises que dans la fonction et pas en dehors.
- */
-// function loadData() {
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             var data = JSON.parse(this.responseText);
-//             parseLinks(data);
-//             console.log("ok");
-//             // drawChart(data);
-//             // updateStatistics(data);
-//         }
-//     };
-//     xhttp.open("GET", "/nodes_link", true);
-//     xhttp.send();
-// }
-//
-// loadData();
-
-
-
-links.forEach(function(link) {
-    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-});
-
-
-var force = d3.layout.force()
-    .nodes(d3.values(nodes))
-    .links(links)
-    .size([width, height])
-    .linkDistance(60)
-    .charge(-300)
-    .on("tick", tick)
-    .start();
-
-var link = svg.selectAll(".link")
-    .data(force.links())
-    .enter().append("line")
-    .attr("class", "link");
-
-var node = svg.selectAll(".node")
-    .data(force.nodes())
-    .enter().append("g")
-    .attr("class", "node")
-    // .on("mouseover", mouseover)
-    // .on("mouseout", mouseout)
-    .on("click", click)
-    .on("dblclick", dblclick)
-
-
-node.append("circle")
-    .attr("r", 8);
-
-node.append("text")
-    .attr("x", 12)
-    .attr("dy", ".35em")
-    .text(function(d) { return d.name; });
+// links.forEach(function(link) {
+//     link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+//     link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+// });
 
 
 function tick() {
@@ -182,7 +59,6 @@ function tick() {
     node
         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 }
-
 
 // action to take on mouse click
 function click() {
@@ -202,9 +78,13 @@ function click() {
 
 function updateNode(node){
 
-    document.getElementById("currentID").innerHTML = (52).toString();
-    document.getElementById("parent").innerHTML = (52).toString();
-    document.getElementById("battery").innerHTML = (52).toString();
+    // document.getElementById("currentID").innerHTML = (52).toString();
+    // document.getElementById("parent").innerHTML = (52).toString();
+    // document.getElementById("battery").innerHTML = (52).toString();
+
+    document.getElementById("currentID").innerHTML = (nodestest[0].ID).toString();
+    document.getElementById("parent").innerHTML = (nodestest[0].parent).toString();
+    document.getElementById("battery").innerHTML = (nodestest[0].battery).toString();
 
 }
 
@@ -230,6 +110,70 @@ function dblclick() {
         .style("font", "10px sans-serif");
     removeNode();
 }
+
+var force;
+var link;
+var node;
+
+var drawTopology = function(data) {
+    force = d3.layout.force()
+        .nodes(d3.values(nodes))
+        .links(links)
+        .size([width, height])
+        .linkDistance(60)
+        .charge(-300)
+        .on("tick", tick)
+        .start();
+
+    link = svg.selectAll(".link")
+        .data(force.links())
+        .enter().append("line")
+        .attr("class", "link");
+
+
+    node = svg.selectAll(".node")
+        .data(force.nodes())
+        .enter().append("g")
+        .attr("class", "node")
+        // .on("mouseover", mouseover)
+        // .on("mouseout", mouseout)
+        .on("click", click)
+        .on("dblclick", dblclick)
+
+    node.append("circle")
+        .attr("r", 8);
+
+    node.append("text")
+        .attr("x", 12)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.name; });
+};
+// drawTopology(); // example of call
+
+
+/*
+ Parse correctement les donnees, les nodes et les links sont bien initialisés. Mais les tableaux ne sont initialises que dans la fonction et pas en dehors.
+ */
+function loadData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            parseLinks(data);
+            drawTopology(data);
+        }
+    };
+    xhttp.open("GET", "/nodes_link", true);
+    xhttp.send();
+}
+
+loadData();
+
+
+
+
+
+
 
 
 
@@ -259,3 +203,69 @@ function dblclick() {
 //         link.type = "onezerozero";
 //     }
 // });
+
+
+
+// var force = d3.layout.force()
+//     .nodes(d3.values(nodes))
+//     .links(links)
+//     .size([width, height])
+//     .linkDistance(60)
+//     .charge(-300)
+//     .on("tick", tick)
+//     .start();
+//
+// var link = svg.selectAll(".link")
+//     .data(force.links())
+//     .enter().append("line")
+//     .attr("class", "link");
+//
+// var node = svg.selectAll(".node")
+//     .data(force.nodes())
+//     .enter().append("g")
+//     .attr("class", "node")
+//     // .on("mouseover", mouseover)
+//     // .on("mouseout", mouseout)
+//     .on("click", click)
+//     .on("dblclick", dblclick)
+//
+//
+// node.append("circle")
+//     .attr("r", 8);
+//
+// node.append("text")
+//     .attr("x", 12)
+//     .attr("dy", ".35em")
+//     .text(function(d) { return d.name; });
+//
+
+// ** Update data section (Called from the onclick)
+// function updateData() {
+//
+//     // Get the data again
+//     d3.csv("data-alt.csv", function(error, data) {
+//         data.forEach(function(d) {
+//             d.date = parseDate(d.date);
+//             d.close = +d.close;
+//         });
+//
+//         // Scale the range of the data again
+//         x.domain(d3.extent(data, function(d) { return d.date; }));
+//         y.domain([0, d3.max(data, function(d) { return d.close; })]);
+//
+//         // Select the section we want to apply our changes to
+//         var svg = d3.select("body").transition();
+//
+//         // Make the changes
+//         svg.select(".line")   // change the line
+//             .duration(750)
+//             .attr("d", valueline(data));
+//         svg.select(".x.axis") // change the x axis
+//             .duration(750)
+//             .call(xAxis);
+//         svg.select(".y.axis") // change the y axis
+//             .duration(750)
+//             .call(yAxis);
+//
+//     });
+// }
--- routes/index.js
+++ routes/index.js
@@ -23,15 +23,46 @@ router.get('/traffic_volume', function(req, res, next) {
   res.send(JSON.stringify(data));
 });
 
+router.get('/nodes_data', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    var data = [
+        {ID : 1, parent : 2, battery : 52},
+    ];
+    res.send(JSON.stringify(data));
+});
+
 router.get('/nodes_link', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   var data = [
-    {source: 1, destination: 2},
-    {source: 1, destination: 3},
-    {source: 2, destination: 4},
-    {source: 3, destination: 4},
-    {source: 4, destination: 5},
-    {source: 5, destination: 6}
+      {source: "ID1", target: "ID2"},
+      {source: "ID1", target: "ID0"},
+      {source: "ID3", target: "ID4"},
+      {source: "ID10", target: "ID4"},
+      {source: "ID5", target: "ID4"},
+      {source: "ID0", target: "ID4"},
+      {source: "ID7", target: "ID4"},
+      {source: "ID4", target: "ID0"},
+      {source: "ID3", target: "ID7"},
+      {source: "ID6", target: "ID7"},
+      {source: "RIM", target: "ID7"},
+      {source: "ID7", target: "ID6"},
+      {source: "ID4", target: "ID5"},
+      {source: "ID9", target: "ID5"},
+      {source: "ID4", target: "ID10"},
+      {source: "ID1", target: "ID10"},
+      {source: "ID10", target: "ID1"},
+      {source: "ID12", target: "ID11"},
+      {source: "ID14", target: "ID11"},
+      {source: "ID7", target: "ID3"},
+      {source: "ID4", target: "ID3"},
+      {source: "ID7", target: "RIM"},
+      {source: "ID5", target: "ID9"}
+    // {source: 1, target: 2},
+    // {source: 1, target: 3},
+    // {source: 2, target: 4},
+    // {source: 3, target: 4},
+    // {source: 4, target: 5},
+    // {source: 4, target: 2}
   ];
   res.send(JSON.stringify(data));
 });
