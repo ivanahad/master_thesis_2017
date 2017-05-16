69225d07a53fd271bb72df38c18fc68d1ee82ce2
ivanahad <ivan.abdelahad@uclouvain.be>
2017-05-16 15:59:51 +0200

    topology presque finie

--- public/javascripts/topology.js
+++ public/javascripts/topology.js
@@ -12,15 +12,9 @@ var svg = d3.select("svg"),
 
 
 
-
-var selectedNode=0;
+// var canClickOnOthers = true;
 var links = [];
 var nodes = [];
-
-var nodestest = [
-    {ID : 1, parent : 2, battery : 52}
-];
-
 
 //Structure : ID, Parent, Battery
 function parseNodes(data){
@@ -28,11 +22,10 @@ function parseNodes(data){
         var IDd = d.ID.toString();
         var parentd = d.parent.toString();
         var batteryd = d.battery.toString();
-        if(parentd!=null) {
+        if(parentd!="none") {
             links.push({source: IDd, target: parentd});
-            nodes[IDd] = {ID: IDd, parent: parentd, battery: batteryd};
         }
-
+        nodes[IDd] = {ID: IDd, parent: parentd, battery: batteryd};
     });
 
     links.forEach(function(link) {
@@ -53,12 +46,18 @@ function tick() {
         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 }
 
+var numberClicked=1;
+var currentnodeid = "";
+var selectednodeid="";
+var aucunassign=true;
+
 // action to take on mouse click
-
-
 function click(node) {
-    console.log(node);
-    if(selectedNode==0) {
+    currentnodeid=node.ID;
+    if(numberClicked % 2 == 1 && aucunassign==true){ //node gets bigger
+        numberClicked++;
+        selectednodeid=currentnodeid;
+        aucunassign=false;
         d3.select(this).select("text").transition()
             .duration(750)
             .attr("x", 22)
@@ -71,39 +70,9 @@ function click(node) {
             .attr("r", 30)
             .style("fill", "lightsteelblue");
         updateNode(node);
-        selectedNode=selectedNode+1;
     }
-}
-
-function updateNode(node){
-
-    // document.getElementById("currentID").innerHTML = (52).toString();
-    // document.getElementById("parent").innerHTML = (52).toString();
-    // document.getElementById("battery").innerHTML = (52).toString();
-    // console.log("updateNode node :"+ node.ID);
-    // document.getElementById("currentID").innerHTML = (nodestest[0].ID).toString();
-    // document.getElementById("parent").innerHTML = (nodestest[0].parent).toString();
-    // document.getElementById("battery").innerHTML = (nodestest[0].battery).toString();
-
-    document.getElementById("currentID").innerHTML = (node.ID).toString();
-    document.getElementById("parent").innerHTML = (node.parent).toString();
-    document.getElementById("battery").innerHTML = (node.battery).toString();
-
-}
-
-function removeNode(){
-
-    document.getElementById("currentID").innerHTML = "-";
-    document.getElementById("parent").innerHTML = "-";
-    document.getElementById("battery").innerHTML = "-" ;
-
-}
-
-
-// action to take on mouse double click
-function dblclick() {
-    if(selectedNode == 1) {
-        console.log(this);
+    else if(numberClicked % 2 !=1 && aucunassign==false && selectednodeid==currentnodeid) {
+        numberClicked++;
         d3.select(this).select("circle").transition()
             .duration(750)
             .attr("r", 8)
@@ -116,8 +85,24 @@ function dblclick() {
             .style("stroke", "none")
             .style("font", "14px sans-serif");
         removeNode();
-        selectedNode = selectedNode-1;
+        aucunassign=true;
+        selectednodeid="";
+
     }
+}
+
+
+function updateNode(node){
+    document.getElementById("currentID").innerHTML = (node.ID).toString();
+    document.getElementById("parent").innerHTML = (node.parent).toString();
+    document.getElementById("battery").innerHTML = (node.battery).toString();
+
+}
+
+function removeNode(){
+    document.getElementById("currentID").innerHTML = "-";
+    document.getElementById("parent").innerHTML = "-";
+    document.getElementById("battery").innerHTML = "-" ;
 }
 
 var force;
@@ -129,7 +114,7 @@ var drawTopology = function() {
         .nodes(d3.values(nodes))
         .links(links)
         .size([width, height])
-        .linkDistance(60)
+        .linkDistance(30)
         .charge(-300)
         .on("tick", tick)
         .start();
@@ -144,7 +129,6 @@ var drawTopology = function() {
         .enter().append("g")
         .attr("class", "node")
         .on("click", click)
-        .on("dblclick", dblclick);
 
     node.append("circle")
         .attr("r", 8);
@@ -153,14 +137,7 @@ var drawTopology = function() {
         .attr("x", 12)
         .attr("dy", ".35em")
         .text(function(d) { return d.ID; });
-        // .text(function(d) { return d.name; });
 };
-
-svg.selectAll(".bar").on('click', function(d, i) {
-    console.log(d);
-    console.log(i);
-    console.log(d3.event);
-});
 
 /*
  Parse correctement les donnees, les nodes et les links sont bien initialisés. Mais les tableaux ne sont initialises que dans la fonction et pas en dehors.
@@ -182,9 +159,14 @@ function loadData() {
 
 loadData();
 
-// var inter = setInterval(function() {
-//     updateData();
-// }, 5000);
+/*
+ todo list :
+
+ - Mettre à jour la topologie en fonction du flux de données reçu (!!!). chaque 5 minutes
+ - changer les couleurs en fonction du dernier message
+ - RDV 14h30 18 may
+ - Modulability of the software
+ */
 
 
 
@@ -282,11 +264,45 @@ loadData();
 //         console.log("source : " + link.source + " target : " + link.target);
 //     });
 // }
-/*
-todo list :
-- ajouter l'opacité sur les links en fonction du volume total échangé ? (optionnel)
-- Mettre à jour la topologie en fonction du flux de données reçu (!!!). chaque 5 minutes
-- changer les couleurs en fonction du dernier message
-- RDV 14h30 18 may
-- Modulability of the software
- */
+
+
+
+// function click(node) {
+//     currentnodeid=node.ID;
+//     if(selectedNodes==0) {
+//         d3.select(this).select("text").transition()
+//             .duration(750)
+//             .attr("x", 22)
+//             .style("fill", "steelblue")
+//             .style("stroke", "lightsteelblue")
+//             .style("stroke-width", ".5px")
+//             .style("font", "20px sans-serif");
+//         d3.select(this).select("circle").transition()
+//             .duration(750)
+//             .attr("r", 30)
+//             .style("fill", "lightsteelblue");
+//         updateNode(node);
+//         selectedNodes=1;
+//     }
+// }
+
+// action to take on mouse double click
+// function dblclick(node) {
+//
+//     if(selectedNodes==1 && node.ID==currentnodeid) {
+//         console.log(this);
+//         d3.select(this).select("circle").transition()
+//             .duration(750)
+//             .attr("r", 8)
+//             .style("fill", "#ccc");
+//         d3.select(this).select("text").transition()
+//             .duration(750)
+//             .attr("x", 12)
+//             .style("stroke", "none")
+//             .style("fill", "black")
+//             .style("stroke", "none")
+//             .style("font", "14px sans-serif");
+//         removeNode();
+//         selectedNodes = 0;
+//     }
+// }
