7c0b0836a89640d855db84faefe785e8b82744e2
Ndizera Eddy <eddyndizera@gmail.com>
2017-06-08 10:11:51 +0200

    display traffic of link on mouseover

--- public/javascripts/topology.js
+++ public/javascripts/topology.js
@@ -18,14 +18,20 @@ function parseNodes(data) {
   const routingTraffic = computeRoutingTraffic(data);
   data.forEach(function(d) {
     d.lastUpdate = d.lastUpdate ? (new Date(d.lastUpdate * 1000)).toLocaleString() : null;
-    d.routing = routingTraffic.nodes[d.id] ? routingTraffic.nodes[d.id] : {octets: 0, packets : 0};
+    d.routing = routingTraffic.nodes[d.id] ? routingTraffic.nodes[d.id] : {
+      octets: 0,
+      packets: 0
+    };
     nodes[d.id] = d;
 
-    const total = d.flows.reduce(function(acc, value){
+    const total = d.flows.reduce(function(acc, value) {
       acc.octets += value.octets;
       acc.packets += value.packets;
       return acc;
-    }, {octets: 0, packets: 0});
+    }, {
+      octets: 0,
+      packets: 0
+    });
     d.total = total;
 
     if (d.parent) {
@@ -40,11 +46,13 @@ function parseNodes(data) {
   });
 
   links.forEach(function(link) {
-    if(link.source in routingTraffic.links && link.target in routingTraffic.links[link.source]){
+    if (link.source in routingTraffic.links && link.target in routingTraffic.links[link.source]) {
       link.traffic = routingTraffic.links[link.source][link.target];
-    }
-    else{
-      link.traffic = {octets: 0, packets : 0};
+    } else {
+      link.traffic = {
+        octets: 0,
+        packets: 0
+      };
     }
     link.source = nodes[link.source] || (nodes[link.source] = {
       name: link.source
@@ -97,8 +105,7 @@ function click(node) {
       .attr("r", 30)
       .style("fill", "lightsteelblue");
     updateNode(node);
-  }
-  else if (selectednodeid == currentnodeid) {
+  } else if (selectednodeid == currentnodeid) {
     d3.select(this).select("circle").transition()
       .duration(750)
       .attr("r", function(d) {
@@ -138,15 +145,15 @@ function updateNode(node) {
   $("#lastsent").html(node.lastUpdate);
 
   const tableFlows = document.getElementById('table_flows');
-  for(let i in node.flows){
+  for (let i in node.flows) {
     const row = tableFlows.insertRow(-1);
     row.insertCell(0).innerHTML = node.flows[i].dst_node === 0 ? "broadcast" : node.flows[i].dst_node;
     row.insertCell(1).innerHTML = node.flows[i].octets;
     row.insertCell(2).innerHTML = node.flows[i].packets;
   }
 
-  $("#total_node").html(node.total.octets.toString() + " bytes ( "+ node.total.packets +")" );
-  $("#total_node_routed").html(node.routing.octets.toString() + " bytes ( "+ node.routing.packets +")" );
+  $("#total_node").html(node.total.octets.toString() + " bytes ( " + node.total.packets + ")");
+  $("#total_node_routed").html(node.routing.octets.toString() + " bytes ( " + node.routing.packets + ")");
 }
 
 function removeNode() {
@@ -157,7 +164,7 @@ function removeNode() {
 
   const tableFlows = document.getElementById('table_flows');
   const tableLength = tableFlows.rows.length;
-  for(let i = 1; i < tableLength; i++){
+  for (let i = 1; i < tableLength; i++) {
     tableFlows.deleteRow(1);
   }
 
@@ -168,6 +175,11 @@ function removeNode() {
 var link;
 var node;
 
+var tooltip = d3.select("body")
+  .append("div")
+  .style("position", "absolute")
+  .style("z-index", "10")
+  .style("visibility", "hidden");
 
 var drawTopology = function() {
   var force = d3.layout.force()
@@ -183,9 +195,18 @@ var drawTopology = function() {
     .data(force.links())
     .enter().append("line")
     .attr("class", "link")
-    .style("stroke-width", function(link){
+    .style("stroke-width", function(link) {
       const width = 1 + Math.floor(link.traffic.octets / 200);
       return width <= 10 ? width : 10;
+    })
+    .on("mouseover", function(link) {
+      return tooltip.style("visibility", "visible").text(link.traffic.octets + " bytes - " + link.traffic.packets + " packets");
+    })
+    .on("mousemove", function() {
+      return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
+    })
+    .on("mouseout", function() {
+      return tooltip.style("visibility", "hidden");
     });
 
   node = svg.selectAll(".node")
@@ -222,23 +243,23 @@ var drawTopology = function() {
     });
 };
 
-function computeMinBattery(data){
-  return data.reduce(function(minBattery, n){
+function computeMinBattery(data) {
+  return data.reduce(function(minBattery, n) {
     return n.battery !== null && minBattery > n.battery ? n.battery : minBattery;
   }, 100);
 }
 
-function computeMaxBattery(data){
-  return data.reduce(function(maxBattery, n){
+function computeMaxBattery(data) {
+  return data.reduce(function(maxBattery, n) {
     return n.battery !== null && maxBattery < n.battery ? n.battery : maxBattery;
   }, 0);
 }
 
-function computeAverageBattery(data){
+function computeAverageBattery(data) {
   var sumBattery = 0;
   var numberNodes = 0;
-  for(let i = 0; i < data.length; i++){
-    if(data[i].battery === null){
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].battery === null) {
       continue;
     }
     sumBattery += data[i].battery;
@@ -247,21 +268,21 @@ function computeAverageBattery(data){
   return numberNodes !== 0 ? sumBattery / numberNodes : 0;
 }
 
-function computeDepth(data){
-  var parents = data.reduce(function(acc, value){
+function computeDepth(data) {
+  var parents = data.reduce(function(acc, value) {
     acc[value.id] = value.parent;
     return acc;
   }, {});
   depths = {};
-  for(let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     let currentNodeId = data[i].id;
     let currentDepth = 0;
-    if(currentNodeId in depths){
+    if (currentNodeId in depths) {
       continue;
     }
     let currentParent = currentNodeId;
     while (parents[currentParent] !== null) {
-      if(currentParent in depths){
+      if (currentParent in depths) {
         currentDepth = 1 + depths[currentParent];
         break;
       }
@@ -273,37 +294,36 @@ function computeDepth(data){
   return depths;
 }
 
-function computePath(src, dst, parents){
+function computePath(src, dst, parents) {
   const srcToGateway = computePathToGateway(src, parents);
   const dstToGateway = computePathToGateway(dst, parents);
 
-  if(dstToGateway.includes(src)){ // dst is a child of scr
+  if (dstToGateway.includes(src)) { // dst is a child of scr
     const index = dstToGateway.indexOf(src);
     return dstToGateway.slice(0, index + 1).reverse();
-  }
-  else if(srcToGateway.includes(dst)){  // src is a child of dst
+  } else if (srcToGateway.includes(dst)) { // src is a child of dst
     const index = srcToGateway.indexOf(dst);
     return srcToGateway.slice(0, index + 1);
   }
-  const indexSrc = srcToGateway.findIndex(function(element){
+  const indexSrc = srcToGateway.findIndex(function(element) {
     return dstToGateway.includes(element);
   });
   const indexDst = dstToGateway.indexOf(srcToGateway[indexSrc]);
   return srcToGateway.slice(0, indexSrc + 1).concat(dstToGateway.slice(0, indexDst).reverse());
 }
 
-function computePathToGateway(src, parents){
+function computePathToGateway(src, parents) {
   const path = [src];
   var currentNode = parents[src];
-  while(currentNode !== null){
+  while (currentNode !== null) {
     path.push(currentNode);
     currentNode = parents[currentNode];
   }
   return path;
 }
 
-function computeRoutingTraffic(data){
-  const parents = data.reduce(function(acc, value){
+function computeRoutingTraffic(data) {
+  const parents = data.reduce(function(acc, value) {
     acc[value.id] = value.parent;
     return acc;
   }, {});
@@ -312,18 +332,18 @@ function computeRoutingTraffic(data){
   var nodesRoutingTraffic = {};
   var linksRouting = {};
 
-  for(let i = 0; i < data.length; i++){
-    for(let j = 0; j < data[i].flows.length; j++){
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].flows.length; j++) {
       const flow = data[i].flows[j];
-      if(flow.src_node === 0 || flow.dst_node === 0){
+      if (flow.src_node === 0 || flow.dst_node === 0) {
         continue;
       }
-      if(!(flow.src_node in routing && flow.dst_node in routing[flow.src_node])){
+      if (!(flow.src_node in routing && flow.dst_node in routing[flow.src_node])) {
         const path = computePath(flow.src_node, flow.dst_node, parents);
-        if(!(flow.src_node in routing)){
+        if (!(flow.src_node in routing)) {
           routing[flow.src_node] = {};
         }
-        if(!(flow.dst_node in routing)){
+        if (!(flow.dst_node in routing)) {
           routing[flow.dst_node] = {};
         }
         routing[flow.src_node][flow.dst_node] = path;
@@ -331,29 +351,38 @@ function computeRoutingTraffic(data){
       }
 
       const routingPath = routing[flow.src_node][flow.dst_node];
-      for(let k = 1; k < routingPath.length - 1; k++){
+      for (let k = 1; k < routingPath.length - 1; k++) {
         const nodeId = routingPath[k];
-        if(!(nodeId in nodesRoutingTraffic)){
-          nodesRoutingTraffic[nodeId] = {octets: 0, packets: 0};
+        if (!(nodeId in nodesRoutingTraffic)) {
+          nodesRoutingTraffic[nodeId] = {
+            octets: 0,
+            packets: 0
+          };
         }
         nodesRoutingTraffic[nodeId].octets += flow.octets;
         nodesRoutingTraffic[nodeId].packets += flow.packets;
       }
 
-      for(let k = 0; k < routingPath.length - 1; k++){
+      for (let k = 0; k < routingPath.length - 1; k++) {
         const src = routingPath[k];
-        const dst = routingPath[k+1];
-        if(!(src in linksRouting)){
+        const dst = routingPath[k + 1];
+        if (!(src in linksRouting)) {
           linksRouting[src] = {};
         }
-        if(!(dst in linksRouting)){
+        if (!(dst in linksRouting)) {
           linksRouting[dst] = {};
         }
-        if(!(dst in linksRouting[src])){
-          linksRouting[src][dst] = {octets: 0, packets: 0};
+        if (!(dst in linksRouting[src])) {
+          linksRouting[src][dst] = {
+            octets: 0,
+            packets: 0
+          };
         }
-        if(!(src in linksRouting[dst])){
-          linksRouting[dst][src] = {octets: 0, packets: 0};
+        if (!(src in linksRouting[dst])) {
+          linksRouting[dst][src] = {
+            octets: 0,
+            packets: 0
+          };
         }
         linksRouting[src][dst].octets += flow.octets;
         linksRouting[src][dst].packets += flow.packets;
@@ -362,10 +391,13 @@ function computeRoutingTraffic(data){
       }
     }
   }
-  return {nodes: nodesRoutingTraffic, links: linksRouting};
+  return {
+    nodes: nodesRoutingTraffic,
+    links: linksRouting
+  };
 }
 
-function computeStats(data){
+function computeStats(data) {
   const minBattery = computeMinBattery(data);
   const maxBattery = computeMaxBattery(data);
   const averageBattery = computeAverageBattery(data);
@@ -374,23 +406,23 @@ function computeStats(data){
   var maxDepth = 0;
 
   const keys = Object.keys(depth);
-  for(let i in keys){
+  for (let i in keys) {
     const key = keys[i];
-    if (depth[key] > maxDepth){
+    if (depth[key] > maxDepth) {
       maxDepth = depth[key];
     }
   }
-  const totalBytes = nodes.reduce(function(acc, value){
+  const totalBytes = nodes.reduce(function(acc, value) {
     return acc + value.total.octets;
   }, 0);
-  const totalPackets = nodes.reduce(function(acc, value){
+  const totalPackets = nodes.reduce(function(acc, value) {
     return acc + value.total.packets;
   }, 0);
 
   document.getElementById("number_nodes").innerHTML = numberNodes;
   document.getElementById("maximum_depth").innerHTML = maxDepth;
   document.getElementById("min_battery").innerHTML = minBattery;
-  document.getElementById("total_bytes").innerHTML = totalBytes.toString() + " bytes ( " + totalPackets +" )";
+  document.getElementById("total_bytes").innerHTML = totalBytes.toString() + " bytes ( " + totalPackets + " )";
 
 }
 
